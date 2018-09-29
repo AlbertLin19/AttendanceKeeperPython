d1b55f95b7dc59dc8c2f8689db338c04dd5f6fca
Albert Lin <Albertkuilin@gmail.com>
2018-09-29 00:45:14 -0700

    implemented creating and accessing csv file for name storage

--- AttendanceKeeper.py
+++ AttendanceKeeper.py
@@ -7,16 +7,41 @@ A simple attendance program for FRC Team 6560 Charging Champions
 @author: Albert Lin
 """
 
-'''
-config setup
-'''
 #%%
 import os
-import configparser
+import csv
 import numpy as np
 
 #%%
-print("Loading settings from local folder...")
-
-config = configparser.ConfigParser()
-config.selections()
+#checking if file of names exists
+nameFile = 'AKNames.csv'
+hasNameFile = False
+for file in os.listdir():
+    if file.__eq__(nameFile):
+        hasNameFile = True
+#if it does not, then create one and initialize
+if not hasNameFile:
+    print(nameFile + " not found, creating one...")
+    newMembers = []
+    done = False
+    while not done:
+        print("type in a member's name to add or type 'q' if done adding members")
+        userIn = input().lower()
+        if userIn.__eq__("q"):
+            done = True
+        else:
+            newMembers.append(userIn)
+            print(newMembers)
+    with open(nameFile, 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',
+                            quotechar='|')
+        filewriter.writerow(newMembers)
+#%%
+#loading names from the csv file
+with open(nameFile, 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    names = []
+    for name in reader:
+        names.append(name)
+    print(names)
+    

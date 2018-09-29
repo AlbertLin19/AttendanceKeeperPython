ac944a1aa8f26f3378f278d43aadb42acd26ba13
Albert Lin <Albertkuilin@gmail.com>
2018-09-29 11:31:20 -0700

    finished working attendance keeper program!

--- AttendanceKeeper.py
+++ AttendanceKeeper.py
@@ -11,7 +11,7 @@ A simple attendance program for FRC Team 6560 Charging Champions
 import os
 import csv
 import numpy as np
-
+import time
 #%%
 #checking if file of names exists
 nameFile = 'AKNames.csv'
@@ -54,31 +54,139 @@ for name in names:
             hasFile = True
     if not hasFile:
         with open(dataFileName, 'w') as csvfile:
-            filewriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+            filewriter = csv.writer(csvfile)
             
 print("Welcome to the Attendance Keeper!")
 
 #%%
 #defining actions
 
-def signIn(self, name):
+def signIn(name):
+    timeIn = time.strftime("%H:%M:%S")[:5]
+    date = time.strftime("%d/%m/%Y")
+    fileName = name + '.csv'
+    #checking if already signed in or not
+    #loading entries
+    with open(fileName, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        entries = []
+        for row in reader:
+            if row.__len__() != 0:
+                entries.append(row)
+    #checking if last entry is on same date
+    sameDay = entries.__len__() != 0 and date.__eq__(entries[-1][0])
     
-def signOut(self, name):
+    #checking to see if you want to override
+    if sameDay:
+        print("There is already an entry - override? (y)es / (n)o")
+        userIn = input().lower()
+        if userIn.__eq__('y'):
+            entries = entries[:-1]
+            shouldInsert = True
+        else:
+            print("not signing in")
+    else:
+        shouldInsert = True
+        
+    #rewriting
+    if shouldInsert:
+        entries.append([date, timeIn])
+        print("Signing " + name + " in at " + timeIn + " on " + date)
+        with open(fileName, 'w') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',', quotechar='|')
+            for row in entries:
+                filewriter.writerow(row)
+def signOut(name):
+    timeOut = time.strftime("%H:%M:%S")[:5]
+    date = time.strftime("%d/%m/%Y")
+    fileName = name + '.csv'
+    #checking if already signed in or not
+    #loading entries
+    with open(fileName, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        entries = []
+        for row in reader:
+            if row.__len__() != 0:
+                entries.append(row)
+    #checking if last entry is on same date
+    sameDay = entries.__len__() != 0 and date.__eq__(entries[-1][0])
+    #checking if already signed out
+    signedOut = entries.__len__() != 0 and entries[-1].__len__() > 2
     
-def show(self, name):
-    
-def add(self, name):
-    
-def remove(self, name):
-
+    if not sameDay:
+        print("you did not sign in yet for today")
+        return
+    if signedOut:
+        print("you are already signed out - override? (y)es / (n)o")
+        usrIn = input().lower()
+        if not usrIn.__eq__('y'):
+            print("not signing out")
+            return
+    print("Signing " + name + " out at " + timeOut + " on " + date)
+    entries[-1] = [entries[-1][0], entries[-1][1], timeOut]
+    with open(fileName, 'w') as csvfile:
+        filewriter = csv.writer(csvfile)
+        for row in entries:
+            filewriter.writerow(row)
+def show(name):
+    fileName = name + '.csv'
+    #loading entries
+    with open(fileName, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        entries = []
+        for row in reader:
+            if row.__len__() != 0:
+                entries.append(row)
+    for row in entries:
+        print(row)
+    totalHrs, numAttendedMeetings = calcTotalHours(name = name)
+    print("total hours: " + str(totalHrs))
+    if numAttendedMeetings != 0:
+        print("average hours spent at an attended meeting: " + str(totalHrs/numAttendedMeetings))
+def showAll():
+    for name in names:
+        print("showing: " + name)
+        fileName = name + '.csv'
+        #loading entries
+        with open(fileName, 'r') as csvfile:
+            reader = csv.reader(csvfile)
+            entries = []
+            for row in reader:
+                if row.__len__() != 0:
+                    entries.append(row)
+        totalHrs, numAttendedMeetings = calcTotalHours(name = name)
+        print("total hours: " + str(totalHrs))
+        if numAttendedMeetings != 0:
+            print("average hours spent at an attended meeting: " + str(totalHrs/numAttendedMeetings))
+        print()
+def calcTotalHours(name):
+    fileName = name + '.csv'
+    hourCount = 0
+    numAttendedMeetings = 0
+    #loading entries
+    with open(fileName, 'r') as csvfile:
+        reader = csv.reader(csvfile)
+        entries = []
+        for row in reader:
+            if row.__len__() != 0:
+                entries.append(row)
+    for row in entries:
+        if row.__len__() == 3:
+            startHr = int(row[1][:2])+int(row[1][3:5])/60
+            endHr = int(row[2][:2])+int(row[2][3:5])/60
+            hourCount+= endHr - startHr
+            numAttendedMeetings += 1
+    return hourCount, numAttendedMeetings
 #%%
 #interactive session
 running = True
 while running:
-    print("Sign (I)n / Sign (O)ut / (S)how Information / (A)dd Member / (R)emove Member / (Q)uit")
+    print("sign (i)n / sign (o)ut / (s)how information / show (a)ll / (q)uit")
     mode = input().lower()
     if mode.__eq__('q'):
         running = False
+    elif mode.__eq__('a'):
+        showAll()
     else:
         print("type in name or 'q' to quit")
         userIn = input().lower()
@@ -87,14 +195,10 @@ while running:
         else:
             if userIn in names:
                 if mode.__eq__('i'):
-                    signIn(userIn)
+                    signIn(name = userIn)
                 elif mode.__eq__('o'):
-                    signOut(userIn)
+                    signOut(name = userIn)
                 elif mode.__eq__('s'):
-                    show(userIn)
-                elif mode.__eq__('a'):
-                    add(userIn)
-                elif mode.__eq__('r'):
-                    remove(userIn)
+                    show(name = userIn)
             else:
                 print("name not in list, returning to mode selection")
